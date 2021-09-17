14f08b6abffcf704b6c08b5280a5fa4a56c2756d
Mahmoud Mohamed Omar Ibrahim <mahmoudelkhashab26@gmail.com>
2021-09-17 19:49:46 +0200

    added buyland (getLand function) separated total from owner functions

--- smartcontract .sol
+++ smartcontract .sol
@@ -10,12 +10,12 @@ contract LNDToken is ERC20 {
     }
     struct Land //creates Land object
     {
-    address ownerAddress; //stores owner address
-    string location; //physical location of property
-    uint value; //cost of property
-    uint landID; //each property is uniquely identifiable
-    
-     }
+        address ownerAddress; //stores owner address
+        string location; //physical location of property
+        uint value; //cost of property
+        uint landID; //each property is uniquely identifiable
+        
+    }
     address public owner; //address of whoever deployed the contract
     uint public totallandsCounter; //total no of lands via this contract at any time
     //land addition event
@@ -36,6 +36,7 @@ contract LNDToken is ERC20 {
     mapping (uint => Land) public __lanidlist;
     mapping(address => mapping(uint => Land)) public _ownerlands;
     mapping(address => uint) public __nooflands;
+    
     function registerLand(string memory _location, uint _cost) public isOwner
     {
         totallandsCounter++;
@@ -65,6 +66,7 @@ contract LNDToken is ERC20 {
                      _ownerlands[_landBuyer][_landID]= myLand;
                      __nooflands[_landBuyer]++;
                      __nooflands[msg.sender]--;
+                     
                      approve(msg.sender,__lanidlist[_landID].value);
                      uint256 newvalue=__lanidlist[_landID].value;
                      /*approve(_landBuyer,newvalue);
@@ -77,18 +79,52 @@ contract LNDToken is ERC20 {
             }
             return false;
     }
-    
-   function getLand(address _landHolder, uint _index) public view returns (string memory, uint, address , uint)
+   function getLand(address _landSeller, uint _landID){
+       if(__lanidlist[_landID].ownerAddress==_landSeller && balanceOf(msg.sender)>=__lanidlist[_landID].value){
+                  Land memory myLand = Land(
+                  {
+                        ownerAddress: msg.sender,
+                        location: __lanidlist[_landID].location,
+                        value: __lanidlist[_landID].value,
+                        landID: _landID
+                    });
+                     __lanidlist[_landID] = myLand;
+                     _ownerlands[msg.sender][_landID]= myLand;
+                     __nooflands[msg.sender]++;
+                     __nooflands[_landSeller]--;
+                     approve(_landSeller,__lanidlist[_landID].value);
+                     uint256 newvalue=__lanidlist[_landID].value;
+                     /*approve(_landBuyer,newvalue);*/
+                    transferFrom(msg.sender,_landSeller,newvalue);
+                    //_transfer(_landBuyer,msg.sender,newvalue);
+                    //remove land from current ownerAddress - assigns everything to 0
+                    delete _ownerlands[_landSeller][_landID];
+                    //emit Transfer(msg.sender, _landBuyer, _landID); //inform the network
+                    return true; 
+            }
+            return false;
+   }
+   function returnLand_owner(address _landHolder, uint _index) public view returns (string memory, uint, address , uint)
     {
         return(_ownerlands[_landHolder][_index].location,
                _ownerlands[_landHolder][_index].value,
                _ownerlands[_landHolder][_index].ownerAddress,
                _ownerlands[_landHolder][_index].landID);
     }
+    function returnLand(uint _index) public view returns (string memory, uint, address , uint)
+    {
+        return(__lanidlist[_index].location,
+               __lanidlist[_index].value,
+               __lanidlist[_index].ownerAddress,
+               __lanidlist[_index].landID);
+    }
     
     //4. GET TOTAL NUMBER OF LANDS OWNED BY AN ACCOUNT
-    function getNoOfLands(address _landHolder) public view returns (uint)
+    function getNoOfLands_owner(address _landHolder) public view returns (uint)
     {
         return __nooflands[_landHolder];
+    }
+    function getNoOfLands() public view returns(uint){
+        return totallandsCounter;
     }
 }
